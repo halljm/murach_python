380580adb9c83674d50238ed81c790cf6f4ebd54
halljm <77579136+halljm@users.noreply.github.com>
2021-01-18 12:09:06 -0600

    finished_2_1

--- exercises/ch02/mpg.py
+++ exercises/ch02/mpg.py
@@ -7,15 +7,18 @@ print()
 # get input from the user
 miles_driven= float(input("Enter miles driven:\t\t"))
 gallons_used = float(input("Enter gallons of gas used:\t"))
+cost_gal = float(input("Enter cost per gal:\t"))
 
 # calculate miles per gallon
-mpg = miles_driven / gallons_used
-mpg = round(mpg, 2)
+# mpg = miles_driven / gallons_used
+mpg = round((miles_driven / gallons_used), 1)
+
+#calc cost per mile
+cost_mile = float((cost_gal * gallons_used) / miles_driven)
             
 # format and display the result
 print()
 print("Miles Per Gallon:\t\t" + str(mpg))
 print()
-print("Bye")
-
-
+print("Total Gas Cost:\t\t" + str(cost_gal * gallons_used))
+print("Cost per mile:\t\t" + str(cost_mile))
