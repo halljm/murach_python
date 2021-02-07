19fa17e25fbb07cd288964b01b3cb628e2286edf
halljm <77579136+halljm@users.noreply.github.com>
2021-02-07 09:35:25 -0600

    finished with chapter 7

--- exercises/ch07/mpg.py
+++ exercises/ch07/mpg.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import csv
+
 def get_miles_driven():
     while True:
         miles_driven = float(input("Enter miles driven :     "))                    
@@ -18,10 +20,32 @@ def get_gallons_used():
             print("Entry must be greater than zero. Please try again.\n")
             continue
 
+def write_trips(trips):
+    #write data from trips to csv named trips.csv
+    with open("trips.csv", "w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerows(trips)
+
+def read_trips():
+    #reads data from trips.csv file and returns a list names trips
+    trips = []
+    with open("trips.csv", newline="") as file:
+        reader = csv.reader(file)
+        for row in reader:
+            trips.append(row)
+    return trips
+
+def list_trips(trips):
+    for trip in trips:
+        print(trip)
+
+
 def main():
     # display a welcome message
     print("The Miles Per Gallon application")
     print()
+    trips = read_trips()
+    list_trips(trips)
 
     more = "y"
     while more.lower() == "y":
@@ -31,9 +55,16 @@ def main():
         mpg = round((miles_driven / gallons_used), 2)
         print("Miles Per Gallon:\t" + str(mpg))
         print()
+
+        trip = [str(miles_driven), str(gallons_used), str(mpg)]
+        trips.append(trip)
+        list_trips(trips)
+        print()
+        print()
         
         more = input("More entries? (y or n): ")
     
+    write_trips(trips)
     print("Bye")
 
 if __name__ == "__main__":
--- exercises/ch07/mpg_write.py
+++ exercises/ch07/mpg_write.py
@@ -1,4 +1,6 @@
-#!/usr/bin/env python3
+100#!/usr/bin/env python3
+
+import csv
 
 def get_miles_driven():
     while True:
@@ -17,11 +19,15 @@ def get_gallons_used():
         else:
             print("Entry must be greater than zero. Please try again.\n")
             continue
-        
+
+def store_data(trip, trips):
+    trips.append(trip)
+
 def main():
     # display a welcome message
     print("The Miles Per Gallon application")
     print()
+    trips = [["Distance", "Gallons", "MPG"]]
 
     more = "y"
     while more.lower() == "y":
@@ -31,9 +37,19 @@ def main():
         mpg = round((miles_driven / gallons_used), 2)
         print("Miles Per Gallon:\t" + str(mpg))
         print()
+
+        #create a list called "trip" from miles_driven, gallons_used and mpg
+        trip = [str(miles_driven), str(gallons_used), str(mpg)]
+
+        #call add to list function and pass the list
+        store_data(trip, trips)
         
         more = input("More entries? (y or n): ")
     
+    #store data to csv file upon exit
+    with open("trips.csv", "w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerows(trips)
     print("Bye")
 
 if __name__ == "__main__":
